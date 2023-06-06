d893f674431668888de2504cb137e6694b63d8a5
Anas Zenagui <zenaguianas20@gmail.com>
2023-06-06 17:17:59 +0100

    Making the graph wighted

--- GraphTheoryApp.py
+++ GraphTheoryApp.py
@@ -5,6 +5,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import ttk
 from tkinter import messagebox
 
+
 def run_algorithm(algorithm):
     start_node = start_node_var.get()
 
@@ -28,6 +29,7 @@ def run_algorithm(algorithm):
         dijkstra_result = nx.single_source_dijkstra(graph, start_node)
         results_label.config(text=f"Dijkstra Result: {dijkstra_result[0]}")
 
+
 def generate_graph():
     global graph
     graph_type = var_graph_type.get()
@@ -40,10 +42,16 @@ def generate_graph():
 
     lines = text_area.get("1.0", tk.END).strip().split("\n")
     for line in lines:
-        node1, node2 = line.strip().split()
+        items = line.strip().split()
+        node1, node2 = items[:2]
 
         if weight_type == "Weighted":
-            graph.add_edge(node1, node2, weight=1)
+            if len(items) > 2:
+                weight = float(items[2])
+                graph.add_edge(node1, node2, weight=weight)
+            else:
+                messagebox.showerror("Error", "Please enter weights for all edges.")
+                return
         else:
             graph.add_edge(node1, node2)
 
@@ -62,6 +70,7 @@ def generate_graph():
 
     nodes = list(graph.nodes())
     start_node_select['values'] = nodes
+
 
 window = tk.Tk()
 window.title("Graph Algorithm GUI")
@@ -107,7 +116,7 @@ start_node_var = tk.StringVar()
 start_node_select = ttk.Combobox(start_node_frame, textvariable=start_node_var)
 start_node_select.pack()
 
-text_area_frame = tk.LabelFrame(window, text="Nodes")
+text_area_frame = tk.LabelFrame(window, text="Nodes (Node1 Node2 [Weight])")
 text_area_frame.pack(padx=10, pady=10)
 
 text_area = tk.Text(text_area_frame, width=30, height=10)
@@ -123,82 +132,4 @@ results_label = ttk.Label(window, text="")
 results_label.pack(pady=10)
 
 window.mainloop()
-
-
-
-# import tkinter as tk
-# from tkinter import ttk
-# from tkinter import *
-# import networkx as nx
-# import matplotlib.pyplot as plt
-#
-# window = tk.Tk()
-# window.title("Graph Application")
-# window.geometry("800x800")
-#
-# # label = tk.Label(window, text="Graph", padx=20, pady=20)
-# # # label.grid(row=0, column=0)
-# # label.grid(row=0, column=0, columnspan=2)
-# #
-# # username_entry = tk.Entry(window)
-# # username_entry.grid(row=0, column=2)
-#
-# frame = tk.Frame(window)
-# frame.pack(pady=20)
-#
-# def drawGraph():
-#     print("HI")
-#
-# labelOfFrame = tk.LabelFrame(frame, text="Data input", padx=30, pady=30)
-# labelOfFrame.grid(row=0, column=0)
-#
-# matrixLabel = tk.Label(labelOfFrame, text="Matrix")
-# matrixLabel.grid(row=1, column=0)
-#
-# matrixInput = tk.Text(labelOfFrame, width=5, height=5, padx=40)
-# matrixInput.grid(row=1, column=1)
-#
-# graphTypeFrame = tk.LabelFrame(frame, text="Graph Type", padx=30)
-# graphTypeFrame.grid(row=0, column=3)
-#
-# radioboxTitle = tk.Radiobutton(graphTypeFrame, text="Directed")
-# radioboxTitle.grid(row=1, column=3)
-#
-# radioboxTitle = tk.Radiobutton(graphTypeFrame, text="Undirected")
-# radioboxTitle.grid(row=2, column=3)
-#
-# submitButton = tk.Button(labelOfFrame, text="Generate", command=drawGraph)
-# submitButton.grid(row=1, column=4, padx=30)
-#
-# graphFormTitle = tk.Label(window, text="Graph Representation")
-# graphFormTitle.pack(pady=20)
-# graphForm = Canvas(window, width=500, height=350, bg="white")
-# graphForm.pack()
-#
-# AlgorithmsFrame = tk.Frame(window)
-# AlgorithmsFrame.pack(pady=20)
-#
-# algorithmesTitle = Label(AlgorithmsFrame, text="Choose an Algorithm")
-# algorithmesTitle.grid(row=0, column=1, padx=30)
-#
-# algorithmes_combobox = ttk.Combobox(AlgorithmsFrame, values=["Kruskal", "BFS", "DFS"])
-# algorithmes_combobox.grid(row=1, column=1, padx=30)
-#
-# calculateButton = tk.Button(AlgorithmsFrame, text="Generate")
-# calculateButton.grid(row=1, column=3, padx=30)
-#
-#
-#
-#
-# window.mainloop()
-
-
-
-
-
-
-
-
-
-
 
